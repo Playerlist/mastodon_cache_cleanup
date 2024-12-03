9c245aaa71996f781ff62123db3058b8f8da9521
Playerlist <playerlist1234@gmail.com>
2024-12-03 16:10:13 +0100

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -4,4 +4,6 @@ echo "Current date and time: $current_date_time" >> cleanuplog.txt
 echo "Cleaning up mastodon cash" >> cleanuplog.txt
 /usr/local/bin/docker-compose run --rm web bundle exec bin/tootctl media remove >> cleanuplog.txt
 /usr/local/bin/docker-compose run --rm web bundle exec bin/tootctl preview_cards remove >> cleanuplog.txt
+/usr/local/bin/docker-compose run --rm web bundle exec bin/tootctl emoji purge --remote-only >> cleanup.txt
+/usr/local/bin/docker-compose run --rm web bundle exec bin/tootctl media remove-orphans >> cleanup.txt
 echo "Done cleaning up" >> cleanuplog.txt
